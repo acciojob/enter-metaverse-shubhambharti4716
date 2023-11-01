cd376e1bbd15cd1aba28d9c9341d6ed5fdfb6ad4
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-01 16:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,5 @@ const button=document.getElementById("enterBtn");
 const final=document.getElementById("final");
 button.addEventListener("click", function (){
 	pTag.textContent="Entered Metaverse";
-	const h1Element = document.createElement("h1");
-    h1Element.textContent = pTag.textContent;
-
-    // Append the h1 element to the "final" element
-    final.appendChild(h1Element);
+	final.textContent=pTag.textContent;
 });
